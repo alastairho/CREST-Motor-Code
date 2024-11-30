9152100d26b6430b5a2dea45e1e16f35c4c9f1c5
alastairho <157694059+alastairho@users.noreply.github.com>
2024-11-30 22:35:35 +0000

    Improvement Attempt 1
    Changing the code and using functions for repeatability.
    
    +Added functions magnetPairOne/Two/Three
    +Added the ability to change the delay using the potentiometer
    
    *Code is not yet tested

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -1,182 +1,150 @@
 //coil 1
-int m1p1 = 4;
-int m1p2 = 2;
-int EN1 = 3;
-float strength1 = 0;
+byte MOTOR1_IN1 = 4;
+byte MOTOR1_IN2 = 2;
+byte ENABLE1 = 3;
 
 //coil 2
-int m2p1 = 8;
-int m2p2 = 7;
-int EN2 = 5;
-float strength2 = 0;
+byte MOTOR2_IN1 = 8;
+byte MOTOR2_IN2 = 7;
+byte ENABLE2 = 5;
 
 //coil 3
-int m3p1 = 12;
-int m3p2 = 13;
-int EN3 = 6;
-float strength3 = 0;
+byte MOTOR3_IN1 = 12;
+byte MOTOR3_IN2 = 13;
+byte ENABLE3 = 6;
 
 //coil 4
-int m4p1 = A1; 
-int m4p2 = A0;
-int EN4 = 9;
-float strength4 = 0;
+byte MOTOR4_IN1 = A1;
+byte MOTOR4_IN2 = A0;
+byte ENABLE4 = 9;
 
 //coil 5
-int m5p1 = A2;
-int m5p2 = A3;
-int EN5 = 10;
-float strength5 = 0;
+byte MOTOR5_IN1 = A2;
+byte MOTOR5_IN2 = A3;
+byte ENABLE5 = 10;
 
 //coil 6
-int m6p1 = A4;
-int m6p2 = A5;
-int EN6 = 11;
-float strength6 = 0;
+byte MOTOR6_IN1 = A4;
+byte MOTOR6_IN2 = A5;
+byte ENABLE6 = 11;
 
-uint32_t period = 1000L; // Speed
+uint16_t period = 1000L; // Speed
 
-int potentiometer = A7;
-int value = 0;
-int time = 200;
-int step = 10;
-int maxSpeed = 20;
+byte potentiometer = A7;
+byte value = 0;
+byte currentDelay = 200;
+byte increment = 10;
+byte finalDelay = 20;
+
+void magnetPairOne(byte voltage,byte polarity){
+  analogWrite(ENABLE1,voltage);
+  analogWrite(ENABLE4,voltage);
+  
+  if(polarity == 0){
+    digitalWrite(MOTOR1_IN1,HIGH);
+    digitalWrite(MOTOR1_IN2,LOW);
+    digitalWrite(MOTOR4_IN1,LOW);
+    digitalWrite(MOTOR4_IN2,HIGH);
+  }
+  if(polarity == 1){
+    digitalWrite(MOTOR1_IN1,LOW);
+    digitalWrite(MOTOR1_IN2,HIGH);
+    digitalWrite(MOTOR4_IN1,HIGH);
+    digitalWrite(MOTOR4_IN2,LOW);
+  }
+}
+
+void magnetPairTwo(byte voltage,byte polarity){
+  analogWrite(ENABLE2,voltage);
+  analogWrite(ENABLE5,voltage);
+  
+  if(polarity == 0){
+    digitalWrite(MOTOR2_IN1,HIGH);
+    digitalWrite(MOTOR2_IN2,LOW);
+    digitalWrite(MOTOR5_IN1,LOW);
+    digitalWrite(MOTOR5_IN2,HIGH);
+  }
+  if(polarity == 1){
+    digitalWrite(MOTOR2_IN1,LOW);
+    digitalWrite(MOTOR2_IN2,HIGH);
+    digitalWrite(MOTOR5_IN1,HIGH);
+    digitalWrite(MOTOR5_IN2,LOW);
+  }
+}
+
+void magnetPairThree(byte voltage,byte polarity){
+  analogWrite(ENABLE3,voltage);
+  analogWrite(ENABLE6,voltage);
+  
+  if(polarity == 0){
+    digitalWrite(MOTOR3_IN1,HIGH);
+    digitalWrite(MOTOR3_IN2,LOW);
+    digitalWrite(MOTOR6_IN1,LOW);
+    digitalWrite(MOTOR6_IN2,HIGH);
+  }
+  if(polarity == 1){
+    digitalWrite(MOTOR3_IN1,LOW);
+    digitalWrite(MOTOR3_IN2,HIGH);
+    digitalWrite(MOTOR6_IN1,HIGH);
+    digitalWrite(MOTOR6_IN2,LOW);
+  }
+}
 
 void setup() {
-  pinMode(m1p1, OUTPUT);
-  pinMode(m1p2, OUTPUT);
-  pinMode(EN1, OUTPUT);
+  pinMode(MOTOR1_IN1, OUTPUT);
+  pinMode(MOTOR1_IN2, OUTPUT);
+  pinMode(ENABLE1, OUTPUT);
 
-  pinMode(m2p1, OUTPUT);
-  pinMode(m2p1, OUTPUT);
-  pinMode(EN2, OUTPUT);
+  pinMode(MOTOR2_IN1, OUTPUT);
+  pinMode(MOTOR2_IN2, OUTPUT);
+  pinMode(ENABLE2, OUTPUT);
 
-  pinMode(m3p1, OUTPUT);
-  pinMode(m3p2, OUTPUT);
-  pinMode(EN3, OUTPUT);
+  pinMode(MOTOR3_IN1, OUTPUT);
+  pinMode(MOTOR3_IN2, OUTPUT);
+  pinMode(ENABLE3, OUTPUT);
 
-  pinMode(m4p1, OUTPUT);
-  pinMode(m4p2, OUTPUT);
-  pinMode(EN4, OUTPUT);
+  pinMode(MOTOR4_IN1, OUTPUT);
+  pinMode(MOTOR4_IN2, OUTPUT);
+  pinMode(ENABLE4, OUTPUT);
 
-  pinMode(m5p1, OUTPUT);
-  pinMode(m5p1, OUTPUT);
-  pinMode(EN5, OUTPUT);
+  pinMode(MOTOR5_IN1, OUTPUT);
+  pinMode(MOTOR5_IN2, OUTPUT);
+  pinMode(ENABLE5, OUTPUT);
 
-  pinMode(m6p1, OUTPUT);
-  pinMode(m6p2, OUTPUT);
-  pinMode(EN6, OUTPUT);
-
-  analogWrite(EN1, 255);
-  analogWrite(EN2, 255);
-  analogWrite(EN3, 255);
-  analogWrite(EN4, 255);
-  analogWrite(EN5, 255);
-  analogWrite(EN6, 255);
-
-  digitalWrite(m2p1,HIGH);
-  digitalWrite(m2p2, LOW);
-  delay(5000);
-
+  pinMode(MOTOR6_IN1, OUTPUT);
+  pinMode(MOTOR6_IN2, OUTPUT);
+  pinMode(ENABLE6, OUTPUT);
+  
   pinMode(potentiometer, INPUT);
 
+  magnetPairOne(255,0);
+  delay(5000);
 }
 
 void loop() {
-
   value = analogRead(potentiometer);
-  uint32_t period = map(value, 0, 255, 10000, 0); 
+  finalDelay = map(value, 0, 1023, 100, 20); 
 
+  magnetPairOne(255,0);
+  magnetPairTwo(0,0);
+  magnetPairThree(0,0);
+  delay(currentDelay);
+  magnetPairOne(0,0);
+  magnetPairTwo(255,0);
+  magnetPairThree(0,0);
+  delay(currentDelay);
+  magnetPairOne(0,0);
+  magnetPairTwo(0,0);
+  magnetPairThree(255,0);
+  delay(currentDelay);
 
-
-  digitalWrite(m1p1,HIGH);
-  digitalWrite(m1p2, LOW);
-  digitalWrite(m4p1,LOW);
-  digitalWrite(m4p2, LOW);
-  digitalWrite(m2p1,LOW);
-  digitalWrite(m2p2, LOW);
-  digitalWrite(m5p1,LOW);
-  digitalWrite(m5p2, LOW);
-  digitalWrite(m3p1,LOW);
-  digitalWrite(m3p2, LOW);
-  digitalWrite(m6p1,LOW);
-  digitalWrite(m6p2, LOW);
-  delay(time);
-
-  digitalWrite(m1p1,LOW);
-  digitalWrite(m1p2, LOW);
-  digitalWrite(m4p1,LOW);
-  digitalWrite(m4p2, LOW);
-  digitalWrite(m2p1,HIGH);
-  digitalWrite(m2p2, LOW);
-  digitalWrite(m5p1,LOW);
-  digitalWrite(m5p2, LOW);
-  digitalWrite(m3p1,LOW);
-  digitalWrite(m3p2, LOW);
-  digitalWrite(m6p1,LOW);
-  digitalWrite(m6p2, LOW);
-  delay(time);
-
-  digitalWrite(m1p1,LOW);
-  digitalWrite(m1p2, LOW);
-  digitalWrite(m4p1,LOW);
-  digitalWrite(m4p2, LOW);
-  digitalWrite(m2p1,LOW);
-  digitalWrite(m2p2, LOW);
-  digitalWrite(m5p1,LOW);
-  digitalWrite(m5p2, LOW);
-  digitalWrite(m3p1,HIGH);
-  digitalWrite(m3p2, LOW);
-  digitalWrite(m6p1,LOW);
-  digitalWrite(m6p2, LOW);
-  delay(time);
-
-  digitalWrite(m1p1,LOW);
-  digitalWrite(m1p2, LOW);
-  digitalWrite(m4p1,HIGH);
-  digitalWrite(m4p2, LOW);
-  digitalWrite(m2p1,LOW);
-  digitalWrite(m2p2, LOW);
-  digitalWrite(m5p1,LOW);
-  digitalWrite(m5p2, LOW);
-  digitalWrite(m3p1,HIGH);
-  digitalWrite(m3p2, LOW);
-  digitalWrite(m6p1,HIGH);
-  digitalWrite(m6p2, LOW);
-  delay(time);
-
-  digitalWrite(m1p1,LOW);
-  digitalWrite(m1p2, LOW);
-  digitalWrite(m4p1,LOW);
-  digitalWrite(m4p2, LOW);
-  digitalWrite(m2p1,LOW);
-  digitalWrite(m2p2, LOW);
-  digitalWrite(m5p1,HIGH);
-  digitalWrite(m5p2, LOW);
-  digitalWrite(m3p1,LOW);
-  digitalWrite(m3p2, LOW);
-  digitalWrite(m6p1,LOW);
-  digitalWrite(m6p2, LOW);
-  delay(time);
-
-  digitalWrite(m1p1,LOW);
-  digitalWrite(m1p2, LOW);
-  digitalWrite(m4p1,LOW);
-  digitalWrite(m4p2, LOW);
-  digitalWrite(m2p1,LOW);
-  digitalWrite(m2p2, LOW);
-  digitalWrite(m5p1,LOW);
-  digitalWrite(m5p2, LOW);
-  digitalWrite(m3p1,HIGH);
-  digitalWrite(m3p2, LOW);
-  digitalWrite(m6p1,HIGH);
-  digitalWrite(m6p2, LOW);
-  delay(time);
-
-  if (time > maxSpeed) {
-    time = time - step;
+  if (currentDelay > finalDelay) {
+    currentDelay = currentDelay - increment;
+  }
+  else if (currentDelay < finalDelay){
+    currentDelay = currentDelay + increment;
   }
   else {
-    time = maxSpeed;
+    currentDelay = finalDelay;
   }
 }
--- README.md
+++ README.md
@@ -4,17 +4,19 @@ This code runs a DIY brushless DC motor. It is uploaded into an Arduino nano. Co
 
 The H-Bridges take in 3 inputs each.
 
-- EN pins - There is **1 EN pin per H-Bridge**. These control the voltage going through the H-Bridge. 0 being none and 255 meaning the full voltage. Thus they need to be controlled using `analogWrite(enablepin,value)`
+- EN pins - There is **1 EN pin per H-Bridge**. These are **enable pins**. They control the voltage going through the H-Bridge. 0 being none and 255 meaning the full voltage. Thus they need to be controlled using `analogWrite(enablepin,value)`
 
-- IN pins - There are **2 IN pins per H-Bridge**. These can be used to control the polarity of the electromagnet by changing the direction of the current. This can be done by using `digitalWrite(inpin,HIGH/LOW)`. For example if you want the current to go one way you can do:
+- IN pins - There are **2 IN pins per H-Bridge**. Lets calls these **input pins**. These can be used to control the polarity of the electromagnet by changing the direction of the current. This can be done by using `digitalWrite(inpin,HIGH/LOW)`. For example if you want the current to go one way you can do:
 ```
-digitalWrite(m1p1,HIGH);
-digitalWrite(m1p2,LOW);
+digitalWrite(MOTOR1_IN1,HIGH);
+digitalWrite(MOTOR1_IN2,LOW);
 ```
 or the other way:
 ```
-digitalWrite(m1p1,LOW);
-digitalWrite(m1p2,HIGH);
+digitalWrite(MOTOR1_IN1,LOW);
+digitalWrite(MOTOR1_IN2,HIGH);
 ```
+
+In the code I have named the enable pins `ENABLE#` and the input pins `MOTOR#_IN#` # being a number.
 
 We also have a potentiometer connected to the Arduino, this changes the speed of the motor.
