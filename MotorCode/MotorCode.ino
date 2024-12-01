e04beeac2aeedaf2132fd85335fae08855f13664
alastairho <157694059+alastairho@users.noreply.github.com>
2024-12-01 00:05:41 +0000

    Used Array
    +Implemented an array for the pin numbers to simplify the code
    +Used iteration to reduce the number of lines

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -1,34 +1,12 @@
-//coil 1
-byte MOTOR1_IN1 = 4;
-byte MOTOR1_IN2 = 2;
-byte ENABLE1 = 3;
-
-//coil 2
-byte MOTOR2_IN1 = 8;
-byte MOTOR2_IN2 = 7;
-byte ENABLE2 = 5;
-
-//coil 3
-byte MOTOR3_IN1 = 12;
-byte MOTOR3_IN2 = 13;
-byte ENABLE3 = 6;
-
-//coil 4
-byte MOTOR4_IN1 = A1;
-byte MOTOR4_IN2 = A0;
-byte ENABLE4 = 9;
-
-//coil 5
-byte MOTOR5_IN1 = A2;
-byte MOTOR5_IN2 = A3;
-byte ENABLE5 = 10;
-
-//coil 6
-byte MOTOR6_IN1 = A4;
-byte MOTOR6_IN2 = A5;
-byte ENABLE6 = 11;
-
-uint16_t period = 1000L; // Speed
+// Motor control pin definitions
+byte motorPins[6][3] = {
+    {4, 2, 3},   // Coil 1: IN1, IN2, ENABLE
+    {8, 7, 5},   // Coil 2
+    {12, 13, 6}, // Coil 3
+    {A1, A0, 9}, // Coil 4
+    {A2, A3, 10},// Coil 5
+    {A4, A5, 11} // Coil 6
+};
 
 byte potentiometer = A7;
 byte value = 0;
@@ -36,108 +14,46 @@ byte currentDelay = 200;
 byte increment = 10;
 byte finalDelay = 20;
 
-void magnetPairOne(byte voltage,byte polarity){
-  analogWrite(ENABLE1,voltage);
-  analogWrite(ENABLE4,voltage);
-  
-  if(polarity == 0){
-    digitalWrite(MOTOR1_IN1,HIGH);
-    digitalWrite(MOTOR1_IN2,LOW);
-    digitalWrite(MOTOR4_IN1,LOW);
-    digitalWrite(MOTOR4_IN2,HIGH);
-  }
-  if(polarity == 1){
-    digitalWrite(MOTOR1_IN1,LOW);
-    digitalWrite(MOTOR1_IN2,HIGH);
-    digitalWrite(MOTOR4_IN1,HIGH);
-    digitalWrite(MOTOR4_IN2,LOW);
-  }
-}
-
-void magnetPairTwo(byte voltage,byte polarity){
-  analogWrite(ENABLE2,voltage);
-  analogWrite(ENABLE5,voltage);
-  
-  if(polarity == 0){
-    digitalWrite(MOTOR2_IN1,HIGH);
-    digitalWrite(MOTOR2_IN2,LOW);
-    digitalWrite(MOTOR5_IN1,LOW);
-    digitalWrite(MOTOR5_IN2,HIGH);
-  }
-  if(polarity == 1){
-    digitalWrite(MOTOR2_IN1,LOW);
-    digitalWrite(MOTOR2_IN2,HIGH);
-    digitalWrite(MOTOR5_IN1,HIGH);
-    digitalWrite(MOTOR5_IN2,LOW);
-  }
-}
-
-void magnetPairThree(byte voltage,byte polarity){
-  analogWrite(ENABLE3,voltage);
-  analogWrite(ENABLE6,voltage);
-  
-  if(polarity == 0){
-    digitalWrite(MOTOR3_IN1,HIGH);
-    digitalWrite(MOTOR3_IN2,LOW);
-    digitalWrite(MOTOR6_IN1,LOW);
-    digitalWrite(MOTOR6_IN2,HIGH);
-  }
-  if(polarity == 1){
-    digitalWrite(MOTOR3_IN1,LOW);
-    digitalWrite(MOTOR3_IN2,HIGH);
-    digitalWrite(MOTOR6_IN1,HIGH);
-    digitalWrite(MOTOR6_IN2,LOW);
-  }
+void setCoilPair(byte motorIndex, byte voltage, byte polarity) {
+    analogWrite(motorPins[motorIndex][2], voltage); // Set ENABLE pin
+    analogWrite(motorPins[motorIndex+3][2], voltage);
+    if (polarity == 0) {
+        digitalWrite(motorPins[motorIndex][0], HIGH); // MOTOR1_IN1, MOTOR2_IN1, MOTOR3_IN1
+        digitalWrite(motorPins[motorIndex][1], LOW);  // MOTOR1_IN2, MOTOR2_IN2, MOTOR3_IN2
+        digitalWrite(motorPins[motorIndex+3][0], LOW);  // MOTOR4_IN1, MOTOR5_IN1, MOTOR6_IN1
+        digitalWrite(motorPins[motorIndex+3][1], HIGH); // MOTOR4_IN2, MOTOR5_IN2, MOTOR6_IN2
+    } else {
+        digitalWrite(motorPins[motorIndex][0], LOW);
+        digitalWrite(motorPins[motorIndex][1], HIGH);
+        digitalWrite(motorPins[motorIndex+3][0], HIGH);
+        digitalWrite(motorPins[motorIndex+3][1], LOW);
+    }
 }
 
 void setup() {
-  pinMode(MOTOR1_IN1, OUTPUT);
-  pinMode(MOTOR1_IN2, OUTPUT);
-  pinMode(ENABLE1, OUTPUT);
-
-  pinMode(MOTOR2_IN1, OUTPUT);
-  pinMode(MOTOR2_IN2, OUTPUT);
-  pinMode(ENABLE2, OUTPUT);
-
-  pinMode(MOTOR3_IN1, OUTPUT);
-  pinMode(MOTOR3_IN2, OUTPUT);
-  pinMode(ENABLE3, OUTPUT);
-
-  pinMode(MOTOR4_IN1, OUTPUT);
-  pinMode(MOTOR4_IN2, OUTPUT);
-  pinMode(ENABLE4, OUTPUT);
-
-  pinMode(MOTOR5_IN1, OUTPUT);
-  pinMode(MOTOR5_IN2, OUTPUT);
-  pinMode(ENABLE5, OUTPUT);
-
-  pinMode(MOTOR6_IN1, OUTPUT);
-  pinMode(MOTOR6_IN2, OUTPUT);
-  pinMode(ENABLE6, OUTPUT);
+  for (byte i = 0; i < 6; i++) {
+      pinMode(motorPins[i][0], OUTPUT); // IN1
+      pinMode(motorPins[i][1], OUTPUT); // IN2
+      pinMode(motorPins[i][2], OUTPUT); // ENABLE
+  }
   
   pinMode(potentiometer, INPUT);
 
-  magnetPairOne(255,0);
+  setCoilPair(0, 255, 0);
   delay(5000);
 }
 
 void loop() {
   value = analogRead(potentiometer);
-  finalDelay = map(value, 0, 1023, 100, 20); 
+  finalDelay = map(value, 0, 1023, 200, 20); //Maps the values of potentiometer into finalDelay values
 
-  magnetPairOne(255,0);
-  magnetPairTwo(0,0);
-  magnetPairThree(0,0);
-  delay(currentDelay);
-  magnetPairOne(0,0);
-  magnetPairTwo(255,0);
-  magnetPairThree(0,0);
-  delay(currentDelay);
-  magnetPairOne(0,0);
-  magnetPairTwo(0,0);
-  magnetPairThree(255,0);
-  delay(currentDelay);
+  for (byte i = 0; i < 3; i++) {
+    setCoilPair(i, 255, 0); // Activate motor pair with polarity 0
+    delay(currentDelay);
+    setCoilPair(i, 0, 0);   // Deactivate motor pair
+  }
 
+//Adjust currentDelay towards finalDelay (Set by potentiometer)
   if (currentDelay > finalDelay) {
     currentDelay = currentDelay - increment;
   }
