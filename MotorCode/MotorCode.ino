f18e4916b4c345a47f00d411ac43b67c371bcaf0
alastairho <157694059+alastairho@users.noreply.github.com>
2025-02-04 20:33:00 +0000

    Working Code

--- MotorCode/MotorCode.ino
+++ MotorCode/MotorCode.ino
@@ -1,14 +1,19 @@
 // Motor control pin definitions
 byte motorPins[6][3] = {
-  {4, 2, 3},   // Coil 1: IN1, IN2, ENABLE
-  {8, 7, 5},   // Coil 2
-  {12, 13, 6}, // Coil 3
-  {A1, A0, 9}, // Coil 4
-  {A2, A3, 10},// Coil 5
-  {A4, A5, 11} // Coil 6
+  {18, 19, 11},   // Coil 1: IN1, IN2, ENABLE
+  {17, 16, 10},   // Coil 2
+  {15, 14, 9}, // Coil 3
+  {13, 12, 6}, // Coil 4
+  {8, 7, 5},// Coil 5
+  {2, 4, 3} // Coil 6
 };
 byte potentiometer = A7;
 byte button = 0;
+byte buttonRead = LOW;
+byte buttonState = LOW;
+byte buttonPrev = LOW;
+unsigned long pressedTime = 0;
+unsigned long debounce = 200UL;
 
 byte increment = 10;
 int currentDelay = 750;
@@ -24,15 +29,15 @@ void setCoil(byte motorIndex, byte voltage, byte polarity) {
   }
 }
 
-void delayUpdate(){
+void delayUpdate() {
+  int finalDelay = 0;
   int value = analogRead(potentiometer);
-  int finalDelay = map(value, 0, 1023, 20, 150); //Maps the values of potentiometer into finalDelay values
-  Serial.println(currentDelay);
 
-  if (finalDelay > 100) {   //Allows 2 different speed modes, demostrator (slow speed), adjustable (high speed)
+  if (value > 950) {   //Allows 2 different speed modes, demostrator (slow speed), adjustable (high speed)
     finalDelay = 750;
   }
   else {
+    finalDelay = map(value, 0, 1023, 20, 150); //Maps the values of potentiometer into finalDelay values
     //Adjust currentDelay towards finalDelay
     if (currentDelay > finalDelay) {
       currentDelay = currentDelay - increment;
@@ -44,32 +49,34 @@ void delayUpdate(){
       currentDelay = finalDelay;
     }
   }
+  //Serial.println(value);
+  //Serial.println(finalDelay);
 }
 
-void permanent(){
+void permanent() {
   for (byte i = 0; i <= 6; i++) {
     long recordedTime = millis();
-    while (currentDelay >= millis()-recordedTime){
+    while (currentDelay >= millis() - recordedTime) {
       setCoil(i, 128, 0); // Activate coil with polarity 0
-      setCoil((i+3)%7, 128, 1);
+      setCoil((i + 3) % 7, 128, 1);
       delayUpdate(); //Increments the currentDelay towards finalDelay
     }
     setCoil(i, 0, 0);   // Deactivate coil
-    setCoil((i+3)%7, 0, 0);
+    setCoil((i + 3) % 7, 0, 0);
     delayUpdate();
   }
 }
 
-void synchronous(){
+void synchronous() {
   for (byte i = 0; i <= 6; i++) {
     long recordedTime = millis();
-    while (currentDelay >= millis()-recordedTime){
-      if (i % 2 == 0){
+    while (currentDelay >= millis() - recordedTime) {
+      if (i % 2 == 0) {
         setCoil(i, 128, 1); // Activate coil with polarity 0
-        setCoil((i+3)%7, 128, 0);
-      }else{
+        setCoil((i + 3) % 7, 128, 0);
+      } else {
         setCoil(i, 128, 0); // Activate coil with polarity 0
-        setCoil((i+3)%7, 128, 1);
+        setCoil((i + 3) % 7, 128, 1);
       }
       delayUpdate(); //Increments the currentDelay towards finalDelay
     }
@@ -86,6 +93,7 @@ void setup() {
   }
 
   pinMode(potentiometer, INPUT);
+  pinMode(button, INPUT);
 
   setCoil(0, 255, 0); //Set motor to starting position
   delay(2500);
@@ -93,10 +101,28 @@ void setup() {
 }
 
 void loop() {
-  if (button == LOW){
+  buttonRead = digitalRead(button);
+  
+  if (buttonRead == HIGH && buttonPrev == LOW && millis() - pressedTime > debounce)
+  {
+    if (buttonState == HIGH){
+      buttonState = LOW;
+    }
+    else {
+      buttonState = HIGH;
+    }
+    pressedTime = millis();
+  }
+
+  if (buttonState == LOW) {
     permanent();
+    Serial.println("Permanet");
   }
-  else{
+  else if (buttonState == HIGH) {
     synchronous();
+    Serial.println("Synchronous");
   }
+
+  buttonPrev = buttonRead;
+
 }
